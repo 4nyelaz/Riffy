bf0b6c99717a4617bb94bfd9b259a9d37d57e735
Angela Vargas <anvaro2006@gmail.com>
2026-04-07 20:13:05 +0200

    feat: estructura inicial del proyecto Riffy

---
+++ src/main/java/riffy/RiffyApplication.java
@@ -0,0 +1,13 @@
+package riffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiffyApplication.class, args);
+	}
+
+}
